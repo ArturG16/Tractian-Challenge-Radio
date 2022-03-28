8c518a0bf40bbda349b26f01147ba0e0a17d732b
ArturG16 <94991828+ArturG16@users.noreply.github.com>
2022-03-28 12:10:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Tractian-Challenge-Radio
+| Supported Targets | ESP32 |
+| ----------------- | ----- |
+
+Tractian Challenge	
+========================
+I developed a full wireless comunication system for to R&D Challenge from Tractian.
+The main objective of this project is send a 500 kB file from a 100 m open-air gap, using at least one battery powered device.
+So to reach that goal I choose to use the Bluetooth LE protocol because it has a low power consumption and great range, also because it is suitable to the new versions of it and different hardwares plataforms.
+
+The hardware that I choose was the ESP32-Pico-D4,which has a solid environment,numerous features with low cost and  which makes this the perfect tool for many of your IoT projects and experiments.
+
+The pcb I made it simple but efficient with the SoC with 4Mb flash built-in and a battery case of CR123A,also I inserted a crystal of 32kHz in esp32 to support the Bluetooth Modem low power clock which make a system has an average current about 2-3mA. That model of battery has an estimated ~1500mAh and a small size.I didn't put in the project any leds or other peripherals because the focus was to reach the main goal to send messages and be realiabe in battery.
+
+
+The firmware I used an example from the manufacturer framework ( ESP - IDF ) which gave me the base to develop a efficient code.
+That example used was the GATT Server, which is ideal to use on sensor's projects. Also that framework has the capability to use FreeRToS which brings to the project a great realiable and provides methods for multiple threads or tasks, mutexes, semaphores and software timers. A tickless mode is provided for low power applications.And also it has greater modularity and less module interdependencies facilitates code reuse across projects. 
+
+Therefore the firmware logic is written below:
+
+In the main code was setup the callbacks,initialization of flash and set the Tx Bluetooth radio at maximum power.To reach the goal of 100meters range.
+```c
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT,ESP_PWR_LVL_P9); 
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9); 
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9); 
+```
+Then I created a while(1) loop with an 20ms inter task delay just to not trigger the watchdog timer.
+
+After that I created a task when the smartphone( or any other device) after pairing with the esp32 ble node, send a notification message to receive the 500kb data from it..
+
+Later on in the ESP_GATTS_WRITE_EVT case event, that part of code is located in inside callback when the node receives a message,so I commented the original routine to send a only message in response and created the task "ble_notify_task"
+
+```c
+xTaskCreate(ble_notify_task, "ble_notify_task", 16384,  &task_arg, 16, NULL); 
+```
+Inside the task the logics consists in read 500bytes of data, which can be a data storaged in flash or read from somewhere else and after that start to send.
+```c
+for (int i = 0; i < sizeof(notify_data); ++i) notify_data[i] = i; 
+ESP_LOGI(GATTS_TAG, "Sending....%d/1024",cnt); // For Debug
+    //the size of notify_data[] need less than MTU size
+esp_ble_gatts_send_indicate(ble_addr[0], ble_addr[1], ble_addr[2],sizeof(notify_data), notify_data, false);
+```
+The time spent to send all the packets was 50seconds.
+
+But the goal of project is send 500kb of data , then I created a counter of each packet of 500bytes was send it and put a limit to 1024 which corresponds a kbyte. After the counter reachs the target number , the loop is break and the task was created is deleted.
+
+```c
+    if(cnt>1023)	notifyDone = true;
+    if(notifyDone==true) break; // Total time spent 51.150 ms.
+	}
+	vTaskDelete(NULL);
+```
+
+Another thing that helped me to simulated if the code was running correctly was to use my Bluetooth Sniffer using the nRF52840 dongle and the ios App from Nordic ( nRF connected ). Which is the reason to insert a 50ms delay inter packets,because if there isn't that the reception will be compromised.
+
+```c
+    vTaskDelay(50/portTICK_RATE_MS); // Task delay necessary to send data correctly.
+```
+
+In that project I didnt used the sleep function because it can be suggestive accordingly with the project, because we can have a numerous ways to wake-up the system.
---
+++ build/main/gatts_demo.d
@@ -0,0 +1,212 @@
+gatts_demo.o: /home/artur/Projects/gatt_server/main/gatts_demo.c \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/FreeRTOS.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_compiler.h \
+ /home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/FreeRTOSConfig.h \
+ /home/artur/Projects/gatt_server/build/include/sdkconfig.h \
+ /home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/xtensa_config.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/hal.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/core.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtensa-versions.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/core-matmap.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/tie.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/system.h \
+ /home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/xtensa_context.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/corebits.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime-frames.h \
+ /home/artur/esp/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/projdefs.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/portable.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/deprecated_definitions.h \
+ /home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/portmacro.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/specreg.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime-core-state.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_private/crosscore_int.h \
+ /home/artur/esp/esp-idf/components/esp_timer/include/esp_timer.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_err.h \
+ /home/artur/esp/esp-idf/components/newlib/platform_include/assert.h \
+ /home/artur/esp/esp-idf/components/soc/include/soc/spinlock.h \
+ /home/artur/esp/esp-idf/components/soc/include/soc/cpu.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/xt_instr_macros.h \
+ /home/artur/esp/esp-idf/components/soc/include/hal/cpu_hal.h \
+ /home/artur/esp/esp-idf/components/soc/include/hal/cpu_types.h \
+ /home/artur/esp/esp-idf/components/soc/src/esp32/include/hal/cpu_ll.h \
+ /home/artur/esp/esp-idf/components/xtensa/include/esp_attr.h \
+ /home/artur/esp/esp-idf/components/soc/soc/esp32/include/soc/cpu_caps.h \
+ /home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/extreg.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_bit_defs.h \
+ /home/artur/esp/esp-idf/components/soc/include/soc/soc_memory_layout.h \
+ /home/artur/esp/esp-idf/components/soc/soc/esp32/include/soc/soc.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_assert.h \
+ /home/artur/esp/esp-idf/components/soc/include/soc/compare_set.h \
+ /home/artur/esp/esp-idf/components/heap/include/esp_heap_caps.h \
+ /home/artur/esp/esp-idf/components/heap/include/multi_heap.h \
+ /home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/portbenchmark.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/mpu_wrappers.h \
+ /home/artur/esp/esp-idf/components/esp_system/include/esp_system.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_idf_version.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/task.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/list.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/event_groups.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/timers.h \
+ /home/artur/esp/esp-idf/components/freertos/include/freertos/task.h \
+ /home/artur/esp/esp-idf/components/log/include/esp_log.h \
+ /home/artur/esp/esp-idf/components/log/include/esp_log_internal.h \
+ /home/artur/esp/esp-idf/components/nvs_flash/include/nvs_flash.h \
+ /home/artur/esp/esp-idf/components/nvs_flash/include/nvs.h \
+ /home/artur/esp/esp-idf/components/spi_flash/include/esp_partition.h \
+ /home/artur/esp/esp-idf/components/spi_flash/include/esp_flash.h \
+ /home/artur/esp/esp-idf/components/soc/include/hal/spi_flash_types.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_types.h \
+ /home/artur/esp/esp-idf/components/soc/include/hal/esp_flash_err.h \
+ /home/artur/esp/esp-idf/components/spi_flash/include/esp_spi_flash.h \
+ /home/artur/esp/esp-idf/components/bt/include/esp_bt.h \
+ /home/artur/esp/esp-idf/components/esp_common/include/esp_task.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatts_api.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_main.h \
+ /home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_common_api.h
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/FreeRTOS.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_compiler.h:
+
+/home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/FreeRTOSConfig.h:
+
+/home/artur/Projects/gatt_server/build/include/sdkconfig.h:
+
+/home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/xtensa_config.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/hal.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/core.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtensa-versions.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/core-matmap.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/tie.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/system.h:
+
+/home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/xtensa_context.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/corebits.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime-frames.h:
+
+/home/artur/esp/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/projdefs.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/portable.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/deprecated_definitions.h:
+
+/home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/portmacro.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/specreg.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xtensa/xtruntime-core-state.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_private/crosscore_int.h:
+
+/home/artur/esp/esp-idf/components/esp_timer/include/esp_timer.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_err.h:
+
+/home/artur/esp/esp-idf/components/newlib/platform_include/assert.h:
+
+/home/artur/esp/esp-idf/components/soc/include/soc/spinlock.h:
+
+/home/artur/esp/esp-idf/components/soc/include/soc/cpu.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/xt_instr_macros.h:
+
+/home/artur/esp/esp-idf/components/soc/include/hal/cpu_hal.h:
+
+/home/artur/esp/esp-idf/components/soc/include/hal/cpu_types.h:
+
+/home/artur/esp/esp-idf/components/soc/src/esp32/include/hal/cpu_ll.h:
+
+/home/artur/esp/esp-idf/components/xtensa/include/esp_attr.h:
+
+/home/artur/esp/esp-idf/components/soc/soc/esp32/include/soc/cpu_caps.h:
+
+/home/artur/esp/esp-idf/components/xtensa/esp32/include/xtensa/config/extreg.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_bit_defs.h:
+
+/home/artur/esp/esp-idf/components/soc/include/soc/soc_memory_layout.h:
+
+/home/artur/esp/esp-idf/components/soc/soc/esp32/include/soc/soc.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_assert.h:
+
+/home/artur/esp/esp-idf/components/soc/include/soc/compare_set.h:
+
+/home/artur/esp/esp-idf/components/heap/include/esp_heap_caps.h:
+
+/home/artur/esp/esp-idf/components/heap/include/multi_heap.h:
+
+/home/artur/esp/esp-idf/components/freertos/xtensa/include/freertos/portbenchmark.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/mpu_wrappers.h:
+
+/home/artur/esp/esp-idf/components/esp_system/include/esp_system.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_idf_version.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/task.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/list.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/event_groups.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/timers.h:
+
+/home/artur/esp/esp-idf/components/freertos/include/freertos/task.h:
+
+/home/artur/esp/esp-idf/components/log/include/esp_log.h:
+
+/home/artur/esp/esp-idf/components/log/include/esp_log_internal.h:
+
+/home/artur/esp/esp-idf/components/nvs_flash/include/nvs_flash.h:
+
+/home/artur/esp/esp-idf/components/nvs_flash/include/nvs.h:
+
+/home/artur/esp/esp-idf/components/spi_flash/include/esp_partition.h:
+
+/home/artur/esp/esp-idf/components/spi_flash/include/esp_flash.h:
+
+/home/artur/esp/esp-idf/components/soc/include/hal/spi_flash_types.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_types.h:
+
+/home/artur/esp/esp-idf/components/soc/include/hal/esp_flash_err.h:
+
+/home/artur/esp/esp-idf/components/spi_flash/include/esp_spi_flash.h:
+
+/home/artur/esp/esp-idf/components/bt/include/esp_bt.h:
+
+/home/artur/esp/esp-idf/components/esp_common/include/esp_task.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatts_api.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_main.h:
+
+/home/artur/esp/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_common_api.h:
+/home/artur/Projects/gatt_server/main/./gatts_demo.c:
